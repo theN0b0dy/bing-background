319df45d1aa6c623c40ed374a140815b2ef662f3
iamAmirrezaSaki <amirreza.m.saki@gmail.com>
2020-12-06 12:06:45 +0330

    fix hardcoded URL.

--- bing-background.sh
+++ bing-background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url="https://www.bing.com/th?id=OHR.BenasqueValley_EN-US1614880060_1920x1080.jpg&rf=LaDigue_1920x1080.jpg"
+url="https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=en-US"
 image_name=$(date '+%Y-%m-%d.jpg') # exmple: [year-month-day.jpg] 2020-05-01.jpg
 image_path=$HOME/Pictures/$image_name # also saving path
 
