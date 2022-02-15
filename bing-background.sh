e6485e2c09ef91eb8e29aa82ca2b82945b053d11
AMS <amirreza.m.saki@gmail.com>
2022-02-15 11:38:23 +0330

    Update bing-background.sh

--- bing-background.sh
+++ bing-background.sh
@@ -4,7 +4,7 @@ url="https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=en-US"
 image_name=$(date '+%Y-%m-%d.jpg') # exmple: [year-month-day.jpg] 2020-05-01.jpg
 image_path=$HOME/Pictures/$image_name # also saving path
 
-#downloading image ...
+#downloading image ..
 wget --output-document=$image_path $url
 
 # changing gnome desktop wallpaper
